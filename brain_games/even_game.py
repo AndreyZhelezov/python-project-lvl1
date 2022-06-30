28821c7bce2ec6ae0695cd844eadc68173749eef
Andrey Zhelezov <azhelezov@mail.ru>
2022-06-30 17:34:46 +0300

    fix: Refactoring brain-even logic

--- brain_games/even_game.py
+++ brain_games/even_game.py
@@ -4,24 +4,24 @@ import prompt
 from random import randint
 
 
-def start_game(user_name: str):
-    correct_count = 0
-    target_correct_count = 3
+def play_game(user_name: str):
+    answer_count = 0
+    target_answer_count = 3
     print('Answer "yes" if the number is even, otherwise answer "no".')
-    while correct_count <= target_correct_count:
-        if correct_count == target_correct_count:
+    while answer_count <= target_answer_count:
+        if answer_count == target_answer_count:
             print(f'Congratulations, {user_name}!')
             sys.exit(0)
         random = randint(1, 100)
         correct_answer = 'yes' if (random % 2) == 0 else 'no'
         print(f'Question: {random}')
         answer = prompt.string('Your answer: ')
-        if answer not in ['yes', 'no'] or answer != correct_answer:
+        if answer in ['yes', 'no'] and answer == correct_answer:
+            print('Correct!')
+            answer_count += 1
+        else:
             print(
                 f'\'{answer}\' is wrong answer ;(. '
                 f'Correct answer was \'{correct_answer}\'.'
             )
             sys.exit(0)
-        else:
-            print('Correct!')
-            correct_count += 1
